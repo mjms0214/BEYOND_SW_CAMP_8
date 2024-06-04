29dc82bef5b34250e5168c046b201f9fe4dea51b
mjms0214 <mjms0214@gmail.com>
2024-06-04 12:26:15 +0900

    feat: add content in A_Variable.java

--- JAVA/01_변수/src/com/beyond/variable/practice/A_Variable.java
+++ JAVA/01_변수/src/com/beyond/variable/practice/A_Variable.java
@@ -1,5 +1,32 @@
 package com.beyond.variable.practice;
 
 public class A_Variable {
-
+	/*
+	 * 변수의 선언
+	 * 변수명: 카멜 표기법
+	 * 	[표현법]
+	 * 		자료형 변수명;
+	 *
+	 */
+	public void variableTest() {
+		/*
+		 * 기본 타: 실제 값이 들어간다.
+		 */
+		// 논리형(boolean)
+		boolean isTrue;
+		
+		// 정수형
+		byte bNum; //(-128 ~ 127)
+		short sNum; //-32,768 ~ 32,767
+		int iNum; //-2,147,483,648 ~ 2,147,483,647
+		long lNum; //-9,223,372,036,854,775,808 ~ 9,223,372,036,854,775,807
+		
+		// 실수형
+		float fNum; //±1.4E-45 ~ 3.4E38
+		
+		// 문자형
+		char ch; //0 ~ 65,535 (유니코드)
+		
+		
+	}
 }
