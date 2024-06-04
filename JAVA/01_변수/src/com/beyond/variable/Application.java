1c0e1ef4b3095a9454b8c0b25311ef88609bf92d
mjms0214 <mjms0214@gmail.com>
2024-06-04 15:27:41 +0900

    fix: execution error

--- JAVA/01_변수/src/com/beyond/variable/Application.java
+++ JAVA/01_변수/src/com/beyond/variable/Application.java
@@ -1,5 +1,7 @@
 package com.beyond.variable;
 
+import com.beyond.variable.practice.A_Variable;
+
 /*
  * 주석(Comment)
  * 	- 소스 코드와는 무관하고, 코드로 인식되지 않는다.
@@ -8,13 +10,26 @@ package com.beyond.variable;
 // 한 줄 주석
 
 // 클래스 명은 자바 파일명과 동일해야 한다.
-public class Application {	
-	
+public class Application {
+
 	// main 메소드는 프로그램의 시작점으로
 	// java.exe가 Application.class를 실행시킬 때 제일 먼저 실행되는 메소드이다.
 	public static void main(String[] args) {
+		// 1) 실행할 메소드가 있는 클래스를 생성(new)한다.
+		//   [표현법]
+		//     클래스명 변수명 = new 클래스명();
+		
+		// 1-1) import 문을 사용하지 않고 클래스 생성
+		// com.beyond.variable.practice.A_Variable variable = new com.beyond.variable.practice.A_Variable();
 
-		System.out.println("실행 테스트");
+		// 1-2) import 문을 사용하여 클래스 생성
+		A_Variable variable = new A_Variable();
+		
+
+		// 2) 생성한 클래스로 메소드 실행(호출)
+		//	[표현법]
+		//		변수명.메소드명()
+		variable.variableTest();
 	}
 
 }
--- JAVA/01_변수/src/com/beyond/variable/practice/A_Variable.java
+++ JAVA/01_변수/src/com/beyond/variable/practice/A_Variable.java
@@ -23,10 +23,88 @@ public class A_Variable {
 		
 		// 실수형
 		float fNum; //±1.4E-45 ~ 3.4E38
+		double dNum;
 		
 		// 문자형
-		char ch; //0 ~ 65,535 (유니코드)
+//		char ch;
+//		char ch = 'A';
+//		char ch = 0;
+//		char ch = '\u0000';
+//		char ch = -90; //0 ~ 65,535 (유니코드)
+//		char ch = 35870;
+		char ch = 'a';
 		
+		System.out.println("ch: " + ch);
+		System.out.println();
+		
+		// 문자열
+//		String name; // 문자열을 가리킬 수 있는 참조 변수를 선언
+//		String name = "";
+//		String name = "홍길동";
+//		String name = new String("홍길동");
+		String name = null; // 참조형 변수에만 null 값을 담을 수 있다.
+		
+		System.out.println("name: " + name);
+		
+		// 지역 변수를 생성한 후 초기화가 되지 않은 경우 에러가 발생한다.
+//		System.out.println(isTrue);
+		
+		isTrue = false; // 선언 후 초기화
+//		isTrue = 1; // 에러 발생
+		
+		System.out.println(isTrue);
+		System.out.println("is True: "+isTrue);
+		System.out.println();
+		
+		// 에러 발생
+//		bNum = 128;
+		bNum = 127;
+//		sNum = 70000;
+		sNum = 32000;
+		iNum = 234567991;
+		// 리터럴은 기본적으로 정수값이므로 int형 범위를 넘어가면 사용할 수 없다.
+//		lNum = 922337203685477580;
+		// long 타입의 리터럴은 숫자 뒤에 L 또는 l을 입력해야 한다.
+		lNum = 922337203685477580L;
+		
+		System.out.println("bNum: " + bNum);
+		System.out.println("sNum: " + sNum);
+		System.out.println("iNum: " + iNum);
+		System.out.println("lNum: " + lNum);
+		System.out.println();
+		
+		// float 타입의 리터럴에서 숫자 뒤에 F 또는 f을 입력해야 한다.
+		fNum = 3.141593F;
+//		dNum = 3.14D;
+		dNum = 3.14;
+		
+		System.out.println("fNum: " + fNum);
+		System.out.println("dNum: " + dNum);
+		
+		// 변수의 명명 규칙
+		// 1) 대소문자 구분
+		int number;
+//		String number; // 자료형이 달라도 변수명이 같으면 에러 발생
+		int NUMBER;
+		int numBer;
+		
+		// 2) 숫자로 시작하면 안 된다.
+//		int 1age;
+		int a1g2e3;
+		
+		// 3) 특수문자는 _, $만 사용이 가능하다.
+		int _age;
+		int ag_e_;
+		int $age;
+		int a$ge$;
+//		int %age;
+//		int age*;
+		
+		// 4) 예악어를 사용하면 안된다.
+//		int double;
+//		int class;
+//		int public;
+//		int void;
 		
 	}
 }
