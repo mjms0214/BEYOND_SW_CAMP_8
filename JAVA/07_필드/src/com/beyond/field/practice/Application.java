75887333bd3835aa3975b7e52e42efe71c9259ab
mjms0214 <mjms0214@gmail.com>
2024-06-10 17:37:14 +0900

    feat: add field practice files

---
+++ JAVA/07_필드/src/com/beyond/field/practice/Application.java
@@ -0,0 +1,23 @@
+package com.beyond.field.practice;
+
+import com.beyond.field.practice.User;
+
+public class Application {
+
+	public static void main(String[] args) {
+		User user = new User();
+
+		// 접근 제한자 테스트
+		// public: 어디서든 필드에 직접 접근이 가능하다.
+		user.id = "kim123";
+		System.out.println(user.id);
+		
+		// default: 같은 패키지 내에서만 직접 접근이 가능하다.
+		user.name = "김철수";
+		System.out.println(user.name);
+		
+		// private: 클래스 내에서만 직접 접근이 가능하다.
+//		System.out.println(user.password);
+	}
+
+}
---
+++ JAVA/07_필드/src/com/beyond/field/practice/User.java
@@ -0,0 +1,15 @@
+package com.beyond.field.practice;
+
+public class User {
+	// 필드 선언
+	public String id = "hong";
+	
+	private String password = "1234";
+	
+	String name = "홍길동";
+	
+	public void test() {
+		
+	}
+
+}
