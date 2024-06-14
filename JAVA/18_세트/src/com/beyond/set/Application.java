066656b64befc29079a64c55e5607a408b019ba5
mjms0214 <mjms0214@gmail.com>
2024-06-14 15:24:15 +0900

    feat: add set practice files

---
+++ JAVA/18_세트/src/com/beyond/set/Application.java
@@ -0,0 +1,14 @@
+package com.beyond.set;
+
+import com.beyond.set.practice.A_HashSet;
+
+public class Application {
+
+	public static void main(String[] args) {
+//		new A_HashSet().method1();
+//		new A_HashSet().method2();
+		new A_HashSet().method3();
+
+	}
+
+}
